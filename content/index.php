376dc3f823e6880f9095dd153bacd14fdb3aede0
ohbster <ohbster@protonmail.com>
2022-04-14 17:22:35 -0500

    Added measure and currency

--- content/index.php
+++ content/index.php
@@ -15,13 +15,16 @@ p   {
 try {
     $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $today = date("Y-m-d");
     //debug echo "Connected to $dbname successfully.";
     #$fmt = numfmt_create('en_US', NumberFormatter::CURRENCY);
 
 
     //debug echo "testing: ";
     //Need to select only values WHERE Day = Today
-    $sql = 'SELECT High FROM GoldPrice';
+    $sql = 'SELECT High, Low, Current, Daystamp FROM GoldPrice
+            WHERE Daystamp = "'.$today.'"' ;
+    
 
     $q = $conn->query($sql);
 
@@ -43,14 +46,18 @@ try {
         <br/>
                 <h1><p>GoldWatch Price Tracker</p></h1>
                 <h1><p>
+                
                 <br/> Current Price: <?php echo '$'.number_format((float) htmlspecialchars($row['Current']),2,'.', '')?>
                 <br/> Daily High: <?php echo '$'.number_format((float) htmlspecialchars($row['High']),2,'.', '')?>
                 <br/> Daily Low: <?php echo '$'.number_format((float) htmlspecialchars($row['Low']),2,'.', '')?>
+                
                 </p></h1>
+                <h3><p><br/>
+                <br/> Prices are in USD per Ounce</p></h4>
 
                 <h4>
                 <br/><br/><p>This page is updated every 5 minutes.
-                <br/>Questions or comments? Feel free to contact ohbster@goldwatch.link
+                <br/>Questions or comments? Feel free to contact <a href ="mailto: ohbster@protonmail.com">ohbster@protonmail.com</a>
                 </p></h4>
 
         </p>

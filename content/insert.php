38e6a865f2816271cf571a9b5ea3316ae4c83760
ohbster <ohbster@protonmail.com>
2022-04-26 17:35:59 -0500

    Added addalert.php and insert.php

---
+++ content/insert.php
@@ -0,0 +1,24 @@
+<?php 
+require_once 'dbconfig.php';
+
+$conn = mysqli_connect("$hostname","$username","$password", "$dbname");
+if ($conn === false){
+    die("Error: Could not connect to database. " . mysqli_connect_error());
+}
+
+$date = new DateTime();
+$time_created = $date->getTimestamp();
+$email = mysql_real_escap_string($conn, $_REQUEST['email']);
+$pricetarget = mysqli_real_escape_string($conn, $_REQUEST['last_name']);
+
+$sql = "INSERT INTO Alerts_High (Email, Price_Target, Time_Created, Last_Checked)
+VALUES ('$email', $pricetarget, $time_created, $time_created)";
+
+if(mysqli_query($conn, $sql)){
+    echo "Alert created!";
+} else {
+    echo "Error: Could not execute $sql. " . mysqli_error($conn);  
+}
+
+mysqli_close($link)
+?>
